d876c71bf47eae65217453b7e470f8870d2a8d67
katratzi <ric_clifford@hotmail.com>
2022-04-21 16:34:01 +0100

    01 i am poor

---
+++ 01 iam poor/i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Richard Clifford on 16/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
