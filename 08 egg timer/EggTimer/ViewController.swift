acd56cb8f6c957ea7466d7535b9d8e21f5b8abf2
katratzi <ric_clifford@hotmail.com>
2023-02-13 03:48:06 +0000

    hook  up buttons in editor and print time

--- 08 egg timer/EggTimer/ViewController.swift
+++ 08 egg timer/EggTimer/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle?.lowercased()
+        if(hardness == "soft")
+        {
+            print(softTime)
+        }
+        else if(hardness == "medium")
+        {
+            print(mediumTime)
+        }
+        else
+        {
+            print(hardTime)
+        }
+        
+    }
+    
 }
