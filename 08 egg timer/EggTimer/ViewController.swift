c1ba64560d7aa88bbb51cf180877061e448b32ef
katratzi <ric_clifford@hotmail.com>
2023-02-13 05:40:54 +0000

    print countdown timer

--- 08 egg timer/EggTimer/ViewController.swift
+++ 08 egg timer/EggTimer/ViewController.swift
@@ -10,26 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet var eggTitle: UILabel?
+    
+    // dictionary of times
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 6]
+    // var dict : [String : Int] : ["Soft": 5, "Medium": 7, "Hard": 12] for explicit typing
+    var timer : Timer?
+    var secondsRemaining : Int = 10
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle?.lowercased()
-        if(hardness == "soft")
+        let hardness = sender.currentTitle!
+        
+        print("Start \(eggTimes[hardness]!)")
+        startTimer(seconds: eggTimes[hardness]!)
+    }
+    
+    func startTimer(seconds : Int) {
+        
+        // start a new count
+        secondsRemaining = seconds
+        print("Countdown started...\(secondsRemaining)")
+        
+        // stop if we have one started
+        if timer != nil
         {
-            print(softTime)
-        }
-        else if(hardness == "medium")
-        {
-            print(mediumTime)
-        }
-        else
-        {
-            print(hardTime)
+            timer!.invalidate()
         }
         
+        // create a timer to fire every second
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            self.updateCounting()
+        })
+    }
+
+    func updateCounting(){
+        print("\(secondsRemaining) seconds.")
+        secondsRemaining-=1
+        if(secondsRemaining < 0)
+        {
+            timer?.invalidate()
+            // change title
+            eggTitle!.text = "Done"
+        }
     }
     
 }
