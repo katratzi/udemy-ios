7fe7d8e5ecbf83403a8e1f6597e0b26e01b1000f
katratzi <ric_clifford@hotmail.com>
2023-02-16 21:33:00 +0000

    finished cyoa

--- 10 destini/Destini-iOS13/Controller/ViewController.swift
+++ 10 destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,15 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
     
     @IBAction func choicePressed(_ sender: UIButton) {
-        print(sender.currentTitle!)
+        
+        storyBrain.nextStory(choice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        
+        storyText.text =  storyBrain.getStoryTitle()
+        buttonOne.setTitle(storyBrain.getChoice1(), for: .normal)
+        buttonTwo.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 }
--- 10 destini/Destini-iOS13/Model/Story.swift
+++ 10 destini/Destini-iOS13/Model/Story.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1 : String
+    var choice2 : String
+    var nextStory1 : Int
+    var nextStory2 : Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.nextStory1 = choice1Destination
+        self.nextStory2 = choice2Destination
+    }
+    
+}
