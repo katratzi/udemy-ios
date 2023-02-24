61b8259dacf9f9b665d03416adb380b2f15828ee
katratzi <ric_clifford@hotmail.com>
2023-02-24 16:03:36 +0000

    moved to swift ui folder

---
+++ 16 swiftui/dicee/dicee/diceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceeApp.swift
+//  dicee
+//
+//  Created by Richard Clifford on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct diceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
