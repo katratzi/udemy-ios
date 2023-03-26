1f15640d8db43021e251c0cc85e7ac0f7b0a7eeb
katratzi <ric_clifford@hotmail.com>
2023-03-27 06:06:17 +0800

    computer and obeserved properties

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -1,9 +1,51 @@
+import Foundation
 
-
-func greeting() {
-    for _ in 0...3 {
-        print("hello")
+// observed property
+var pizzaInInches : Int = 10 {
+    // before change
+    willSet {
+        print(pizzaInInches)
+        print(newValue)
+    }
+    // after change
+    didSet {
+        if pizzaInInches >= 18
+        {
+            print("invalid size, pizza set to 18")
+            pizzaInInches = 18
+        }
     }
 }
 
-greeting()
+var numberOfPeople : Int = 12
+let slicesPerPerson : Int = 6
+
+// computed - must be var
+// have to specify datatype
+var numberOfSlices : Int {
+    // long version of getter
+    get {
+        return pizzaInInches - 4
+    }
+}
+
+var numberOfPizza : Int {
+    get {
+        let numPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfPeople / numPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
+
+numberOfPizza = 4
+print(numberOfPeople)
+
+pizzaInInches = 33
+print(pizzaInInches)
+
+
+
+
--- notes/notes.md
+++ notes/notes.md
@@ -26,3 +26,47 @@ var selectedCategory : Category? {
             loadItems()
         }
     }
+
+# Computer Properties
+
+
+let pizzaInInches : Int = 14
+
+// computed - must be var
+// have to specify datatype
+var numberOfSlices : Int {
+    // short version of getter
+    return pizzaInInches - 4
+}
+
+var numberOfSlices : Int {
+    // long version of getter
+    get {
+        return pizzaInInches - 4
+    }
+    set {
+        // newValue is the keyword
+        print("num of slice is now \(newValue)")
+    }
+}
+
+print(numberOfSlices)
+
+# Observed Properties
+
+// observed property
+var pizzaInInches : Int = 10 {
+    // before change
+    willSet {
+        print(pizzaInInches)
+        print(newValue)
+    }
+    // after change
+    didSet {
+        print(oldValue)
+        print(pizzaInInches)
+    }
+}
+
+
+
