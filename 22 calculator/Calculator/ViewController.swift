4d462d2a09407fed35a1da5ae637e9c26f64265d
katratzi <ric_clifford@hotmail.com>
2023-03-27 08:25:10 +0800

    simple calc buttons

--- 22 calculator/Calculator/ViewController.swift
+++ 22 calculator/Calculator/ViewController.swift
@@ -12,12 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var isFinishedTypingNumber : Bool = true
+    private var isFinishedTypingNumber : Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label to Double")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-"{
+                displayLabel.text = String(number * -1)
+            }
+            else if calcMethod == "AC"{
+                displayLabel.text = "0"
+            }
+            else if calcMethod == "%"{
+                displayLabel.text = String(number/100.0)
+            }
+        }
+        
+        
     }
     
     
