c88b4e47a010b51761d9158f0a126797af910b41
katratzi <ric_clifford@hotmail.com>
2023-03-28 06:52:43 +0800

    getting the image picker working

--- 23 seefood/SeeFood/SeeFood/ViewController.swift
+++ 23 seefood/SeeFood/SeeFood/ViewController.swift
@@ -10,21 +10,40 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-    @IBOutlet weak var imageView: UIImageView!
     
+    
+    @IBOutlet weak var imageView: UIImageView!
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // allows access to the camera to take image for picker
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    // this delegate method is called once the user has finished picking an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // check an image was picked, cast to UIImage then set to background image
+        if let userPickedImage = info[.originalImage] as? UIImage
+        {
+            imageView.image = userPickedImage
+        }
+        
+        // dismiss image picker to go back to main view
+        imagePicker.dismiss(animated: true)
         
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
     }
-
-   
+    
+    
 }
 
