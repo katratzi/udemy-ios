c96c71e6fffffe3eb8c7a304f091a79506cb599f
katratzi <ric_clifford@hotmail.com>
2023-03-30 07:33:36 +0800

    add a dice into our ar view

--- 27 ARDicee/ARDicee/ARDicee/ViewController.swift
+++ 27 ARDicee/ARDicee/ARDicee/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -21,39 +21,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         
         
-        // lets add a simple cube to our scene with a red material
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        cube.materials = [material]
+        //        // lets add a simple cube to our scene with a red material
+        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        //        let material = SCNMaterial()
+        //        material.diffuse.contents = UIColor.red
+        //        cube.materials = [material]
+        //
+        //        // add a sphere
+        //        let sphere = SCNSphere(radius: 0.2)
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpg")
+        //        sphere.materials = [material]
         
-        // add a sphere
-        let sphere = SCNSphere(radius: 0.2)
-        material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpg")
-        sphere.materials = [material]
-        
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        node.geometry = sphere
+        //        let node = SCNNode()
+        //        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //        node.geometry = sphere
         
         // add to our scene
-        sceneView.scene.rootNode.addChildNode(node)
+        //        sceneView.scene.rootNode.addChildNode(node)
+        
+        
+        // Create a new scene for dice
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            diceNode.scale = SCNVector3(10, 10, 10)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        } else {
+            print("dice not found")
+        }
+        
+        // Set the scene to the view
+        // sceneView.scene = diceScene
+        
+        
         sceneView.autoenablesDefaultLighting = true
-        
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
-        
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -64,6 +75,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     
 }
