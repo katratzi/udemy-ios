eec7903633176f23e7bcf3ab55944566cccdf9cd
katratzi <ric_clifford@hotmail.com>
2023-03-30 07:03:27 +0800

    adding the moon into our living room

--- 27 ARDicee/ARDicee/ARDicee/ViewController.swift
+++ 27 ARDicee/ARDicee/ARDicee/ViewController.swift
@@ -19,18 +19,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        
+        
         // lets add a simple cube to our scene with a red material
         let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.red
         cube.materials = [material]
         
+        // add a sphere
+        let sphere = SCNSphere(radius: 0.2)
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_moon.jpg")
+        sphere.materials = [material]
+        
         let node = SCNNode()
         node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        node.geometry = cube
+        node.geometry = sphere
         
+        // add to our scene
         sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
         
     }
     
