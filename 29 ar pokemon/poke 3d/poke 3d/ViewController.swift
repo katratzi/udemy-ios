00527c83863cc533e4c26e460505bbc3b53b4a3f
katratzi <ric_clifford@hotmail.com>
2023-03-31 08:13:38 +0800

    29 pokemon ar app initial commit tracked images added

---
+++ 29 ar pokemon/poke 3d/poke 3d/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  poke 3d
+//
+//  Created by Richard Clifford on 31/03/2023.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            
+            configuration.trackingImages = imagesToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images successfully added")
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    
+}
