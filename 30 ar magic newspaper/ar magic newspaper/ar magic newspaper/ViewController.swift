bf3a6fef39f06428c288128679ec3c5673f43749
katratzi <ric_clifford@hotmail.com>
2023-04-02 07:25:20 +0800

    30 magic newspaper app initial setup

---
+++ 30 ar magic newspaper/ar magic newspaper/ar magic newspaper/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ar magic newspaper
+//
+//  Created by Richard Clifford on 02/04/2023.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Magic News Images", bundle: Bundle.main) {
+            
+            configuration.trackingImages = imagesToTrack
+            configuration.maximumNumberOfTrackedImages = 1
+            
+            print("Images successfully added")
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
